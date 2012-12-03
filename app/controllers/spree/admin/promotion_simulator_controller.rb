1f960da0ee091e527dbad27acaca0e875ef239af
Guruprasad <dv.guruprasad@gmail.com>
2012-12-03 17:25:34 +0530

    added a hardcoded chart

--- app/controllers/spree/admin/promotion_simulator_controller.rb
+++ app/controllers/spree/admin/promotion_simulator_controller.rb
@@ -1,6 +1,6 @@
 module Spree
     module Admin
-        class PromotionSimulatorController < ApplicationController
+        class PromotionSimulatorController < Spree::Admin::BaseController
             def index
                 sales = ProductWeeklySales.sales_including_forecasts(Date.new(2013, 1, 7), 6)
             end
