0ba9b529367d9dc8777a06de06eb341a7493ece1
Pramod N <npramod05@gmail.com>
2012-12-04 12:01:33 +0530

    adding assortmentReport to contain assortment data

--- app/controllers/spree/admin/assortment_map_controller.rb
+++ app/controllers/spree/admin/assortment_map_controller.rb
@@ -15,7 +15,8 @@ module Spree
             def create_chart_data(products_sales_distribution)
                 products_sales_distribution.map do |product_id, distribution|
                     color_value = ColorGenerator.generate(distribution["total_revenue"],distribution["total_target_revenue"])
-                    {"id" => product_id, "value" => distribution["total_revenue"], "label" => Spree::Product.select(:name).where("id = #{product_id}").first.name , "color" => '#' + color_value }
+                    label = Spree::Product.select(:name).where("id = #{product_id}").first.name
+                    AssortmentReport.new(product_id,distribution["total_revenue"],label,'#' + color_value)
                 end
             end
 
