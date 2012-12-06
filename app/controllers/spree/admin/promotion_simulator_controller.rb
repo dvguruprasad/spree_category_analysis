ca9ed0807aaa91ebca2db0bfc93f122e2206bd8d
archanar <archupsgtech@gmail.com>
2012-12-06 15:14:46 +0530

    Adding a bunch of other graphs

--- app/controllers/spree/admin/promotion_simulator_controller.rb
+++ app/controllers/spree/admin/promotion_simulator_controller.rb
@@ -7,12 +7,21 @@ module Spree
                 number_of_weeks = 6
                 sales = WeeklySales.sales_including_forecasts(product, date_of_forecast,number_of_weeks)
                 @sum_target_revenue = sales.sum(&:target_revenue)
+
+                @weekly_target_revenue = sales.map{|s| s.target_revenue}
                 
                 sum_revenue = 0.0
                 @sales_revenue = sales.map{|s| s.revenue}
                 @cumulative_sale = sales.map do |sale|
                     sale.revenue + sum_revenue
                     sum_revenue += sale.revenue
+                end
+
+                @sales_margin = sales.map{|s| s.revenue - s.cost}
+
+                sum_margin = 0.0
+                @cumulative_sales_margin = @sales_margin.map do |sale|
+                  sum_margin += sale
                 end
 
                 @inventory_positions = ProductWeeklyInventoryPosition.inventory_positions(sales).map{|p| p.closing_position}
