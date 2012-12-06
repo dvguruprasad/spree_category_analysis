dcbe166a73f8d3c105a265767b41bc744d0d44d1
rohit kumar <roh422@yahoo.com>
2012-12-06 18:08:38 +0530

    added buttons for weekly view

--- app/assets/javascripts/admin/assortment_map.js
+++ app/assets/javascripts/admin/assortment_map.js
@@ -1,12 +1,12 @@
-function render_tree_map(list,type) {
+function render_tree_map(list, type) {
 
     $('div#treemap-div').treemap(list, {
         backgroundColor:function (node, box) {
             return node.color;
         },
-         click: function(node, event){
-            if(type != "product")
-             window.location.href = "/admin/assortment_map/" + node.id;
-         }
+        click:function (node, event) {
+            if (type != "product")
+                window.location.href = "/admin/assortment_map/" + node.id;
+        }
     });
 }
--- app/controllers/spree/admin/assortment_map_controller.rb
+++ app/controllers/spree/admin/assortment_map_controller.rb
@@ -4,9 +4,9 @@ module Spree
             respond_to :json, :html
 
             def index
-                taxon = Spree::Taxon.find_by_id(params[:taxon_id]) unless params[:taxon_id].nil?
-                return if taxon.nil?
-                weekly_sales_for_taxon = taxon.weekly_sales
+                @taxon = Spree::Taxon.find_by_id(params[:taxon_id]) unless params[:taxon_id].nil?
+                return if @taxon.nil?
+                weekly_sales_for_taxon = @taxon.weekly_sales
                 weekly_aggregate_for_taxon = WeeklySales.generate_aggregates(weekly_sales_for_taxon)
                 if !weekly_aggregate_for_taxon.nil?
                     @data = create_chart_data(weekly_aggregate_for_taxon).to_json
@@ -18,7 +18,6 @@ module Spree
                 sales_distribution.map do |child_id, distribution|
                     color_value = ColorGenerator.generate(distribution["total_revenue"],distribution["total_target_revenue"])
                     label, @type = create_label_for_child(child_id)
-                    p "type value #######  #{@type}"
                     AssortmentReport.new(child_id,distribution["total_revenue"],label,'#' + color_value)
                 end
             end
