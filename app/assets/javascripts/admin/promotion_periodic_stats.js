3b7aed8118ae5d96e2292fcb8c98d0c6f7258aab
rohit kumar <roh422@yahoo.com>
2012-12-21 14:17:56 +0530

    html layout changes

--- app/assets/javascripts/admin/promotion_periodic_stats.js
+++ app/assets/javascripts/admin/promotion_periodic_stats.js
@@ -5,21 +5,23 @@ var show_sales_statistics = function(report){
     var reveneue_variation= document.getElementById('revenue_variation');
     var gross_profit = document.getElementById('gross_profit');
     var growth = document.getElementById('growth');
-    var stock_out_of_date = document.getElementById('stock_out_of_date');
+//    var stock_out_of_date = document.getElementById('stock_out_of_date');
     var revenue_change = document.getElementById('revenue_change');
     var profit_change = document.getElementById('profit_change');
     var simulated_revenue = document.getElementById('simulated_revenue');
     var simulated_profit = document.getElementById('simulated_profit');
+    var simulated_revenue_variation = document.getElementById('simulated_revenue_variation');
 
     total_sales.innerHTML = report.total_sales
     target_sales.innerHTML = report.target_sales
     reveneue_variation.innerHTML = report.revenue_variation
     gross_profit.innerHTML = report.gross_profit
     growth.innerHTML = report.growth_over_previous_period
-    stock_out_of_date.innerHTML = report.stock_out_date
-    revenue_change.innerHTML = report.promotional_revenue_change    
+//    stock_out_of_date.innerHTML = "25/01/2013"
+    revenue_change.innerHTML = report.promotional_revenue_change
     profit_change.innerHTML = report.promotional_profit_change
     simulated_revenue.innerHTML = report.simulated_revenue
     simulated_profit.innerHTML = report.simulated_profit
+    simulated_revenue_variation.innerHTML = report.simulated_revenue_variation
 };
 
--- app/models/spree/admin/periodic_stats.rb
+++ app/models/spree/admin/periodic_stats.rb
@@ -13,18 +13,21 @@ module Spree
                 promotional_profit_change = 0
                 simulated_revenue = 0
                 simulated_profit = 0
+                simulated_revenue_variation = 0
+
                 {:total_sales => total_revenue, :target_sales=>target_revenue, :revenue_variation=>revenue_variation, :gross_profit=>gross_profit,
                  :growth_over_previous_period=>growth_over_previous_period, :stock_out_date=>stock_out_date,
                  :promotional_revenue_change=>promotional_revenue_change,
                  :promotional_profit_change=>promotional_profit_change,:simulated_profit =>simulated_profit,
-                 :simulated_revenue => simulated_revenue}
+                 :simulated_revenue => simulated_revenue,:simulated_revenue_variation =>simulated_revenue_variation}
 
             end
 
             def self.generate_with_promotion(weekly_sales, simulated_sales)
                 total_revenue = WeeklySales.total_revenue(weekly_sales).round(2)
                 target_revenue = WeeklySales.total_target_revenue(weekly_sales).round(2)
-
+                simulated_revenue_sum  = simulated_sales.inject(0) { |sum, s| sum + s.revenue }
+                simulated_revenue_variation = (simulated_revenue_sum - target_revenue).round(2)
                 revenue_variation = (total_revenue - target_revenue ).round(2)
                 total_cost =  WeeklySales.total_cost(weekly_sales)
                 gross_profit = (total_revenue - total_cost).round(2)
@@ -42,7 +45,8 @@ module Spree
                  :growth_over_previous_period=>growth_over_previous_period, :stock_out_date=>stock_out_date,
                  :promotional_revenue_change=>promotional_revenue_change,
                  :promotional_profit_change=>promotional_profit_change,
-                 :simulated_revenue => simulated_revenue, :simulated_profit=> simulated_profit}
+                 :simulated_revenue => simulated_revenue, :simulated_profit=> simulated_profit,
+                 :simulated_revenue_variation =>simulated_revenue_variation}
             end
             def self.revenue_change(total_revenue, simulated_sales)
                 total_simulated_revenue = simulated_sales.each.inject(0.0){|sum,s|sum+s.revenue}
@@ -50,7 +54,7 @@ module Spree
             end
 
             def self.profit_after_simulation(simulated_sales, total_cost)
-                total_simulated_revenue = simulated_sales.each.inject(0){|sum,s|sum+s.revenue}
+                total_simulated_revenue = simulated_sales.inject(0){|sum,s|sum+s.revenue}
                 (total_simulated_revenue - total_cost).round(3)
             end
 
