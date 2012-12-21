f98911253eaff3a94f16c3b0120832c9611ac43a
rohit kumar <roh422@yahoo.com>
2012-12-21 11:33:02 +0530

    enhanced some UI properties of the slider with added validation

--- app/assets/javascripts/admin/script.js
+++ app/assets/javascripts/admin/script.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
-  var startFlag = 0;
+  var startFlag = 0, startDate, endDate,index;
   $(".range-sel-box li a").on("click", function(){
     var parent = $(this).parent();
 
     if(!parent.is(".date-sel") && !parent.is(".date-start") && !parent.is(".date-end")) {
       if(!startFlag) {
         parent.addClass("date-start");
+        startDate =   parseInt(parent.text().trim());
         startFlag = 1;
-      } else {				
-        parent.addClass("date-end");
-        startFlag = 0;
-        text = parent.text().trim();
-        index = text.substring(0,2).trim();
+      } else {
+          endDate = parseInt(parent.text().trim());
 
-        parent.prevUntil(".date-start").addClass("date-sel");
-        alert("#bubble"+index);
+          if(endDate > startDate) {
+              parent.addClass("date-end");
+              startFlag = 0;
+              index = endDate;
+              parent.prevUntil(".date-start").addClass("date-sel");
+          } else {
+              alert("Please select a date after the start date");
+          }
+
         $(".promo-bubble").toggle(false);
         $("#bubble"+index).toggle();
-        prev_id = index;
       }
+    } else if (parent.is(".date-end")) {
+
+        index = parseInt(parent.text().trim());
+        $(".promo-bubble").toggle(false);
+        $("#bubble"+index).toggle();
     }
   });
 });
+
