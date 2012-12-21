cbb52cee881952e1a502bf7b736f2e5829f4215f
archanar <archupsgtech@gmail.com>
2012-12-21 07:09:37 +0530

    Showing and hiding of the promotion input boxes

--- app/assets/javascripts/admin/script.js
+++ app/assets/javascripts/admin/script.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
-	var startFlag = 0;
-	$(".range-sel-box li a").off("click").on("click", function(){
-		var parent = $(this).parent();
+  var startFlag = 0;
+  $(".range-sel-box li a").on("click", function(){
+    var parent = $(this).parent();
 
-		if(!parent.is(".date-sel") && !parent.is(".date-start") && !parent.is(".date-end")) {
-			if(!startFlag) {
-				parent.addClass("date-start");
-				startFlag = 1;
-			} else {				
-				parent.addClass("date-end");
-                startFlag = 0;
-                index = parent.text();
+    if(!parent.is(".date-sel") && !parent.is(".date-start") && !parent.is(".date-end")) {
+      if(!startFlag) {
+        parent.addClass("date-start");
+        startFlag = 1;
+      } else {				
+        parent.addClass("date-end");
+        startFlag = 0;
+        text = parent.text().trim();
+        index = text.substring(0,2).trim();
 
-      				parent.prevUntil(".date-start").addClass("date-sel");
-               html = $("#promotion-type").html();
-                $('.promo-bubble').wrap('<div class=bubble_' +index+' />');
-                console.log(parent);
-                parent.append(html);
-			}
-		}
-    });
+        parent.prevUntil(".date-start").addClass("date-sel");
+        alert("#bubble"+index);
+        $(".promo-bubble").toggle(false);
+        $("#bubble"+index).toggle();
+        prev_id = index;
+      }
+    }
+  });
 });
