ef312e7076cfcbc77a8e72dfa32a139c3a039729
Pramod N <npramod05@gmail.com>
2012-12-31 18:11:35 +0530

    landing page(static) for promotion simulator

--- app/overrides/all.rb
+++ app/overrides/all.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "promotion_simulator",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => '<li><a href="/admin/promotion_simulator/product/1060500711/14/1/2013">Promotion Simulator</a></li>')
+                     :text => '<li><a href="/admin/promotion_simulator/">Promotion Simulator</a></li>')
 #TODO: fix the promo over ride to point to a product list page and not a specific product
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "assortment_map",
