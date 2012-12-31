8cfaaa11a01d450ba82ec332265de27b689ccd5c
Pramod N <npramod05@gmail.com>
2012-12-31 16:32:02 +0530

    capability to remove a promotion

--- app/assets/javascripts/admin/script.js
+++ app/assets/javascripts/admin/script.js
@@ -1,50 +1,68 @@
 $(document).ready(function() {
-  var startFlag = 0, startDate, endDate,index;
-  $(".range-sel-box li a").on("click", function(){
-    var parent = $(this).parent();
+    var promotion_number = 0;
+    var startFlag = 0, startDate, endDate,index;
+    $(".range-sel-box li a").on("click", function(){
+        var parent = $(this).parent();
 
-    if(!parent.is(".date-sel") && !parent.is(".date-start") && !parent.is(".date-end")) {
-      if(!startFlag) {
-        parent.addClass("date-start");
-        startDate =   parseInt(parent.text().trim());
-        startFlag = 1;
-      } else {
-          endDate = parseInt(parent.text().trim());
+        if(!parent.is(".date-sel") && !parent.is(".date-start") && !parent.is(".date-end")) {
+            if(!startFlag) {
+                promotion_number += 1;
+                parent.addClass("date-start");
+                add_promotion_details(parent);
+                startDate =   parseInt(parent.text().trim());
+                startFlag = 1;
+            } else {
+                endDate = parseInt(parent.text().trim());
 
-          if(endDate > startDate) {
-              parent.addClass("date-end");
-              startFlag = 0;
-              index = endDate;
-              parent.prevUntil(".date-start").addClass("date-sel");
-          } else {
-              alert("Please select a date after the start date");
-          }
+                if(endDate > startDate) {
+                    parent.addClass("date-end");
+                    add_promotion_details(parent);
+                    add_promotion_details(parent.find('form.promo-form'));
+                    add_promotion_details(parent.find('form.promo-form input.remove_button'));
+                    $(parent.find('form.promo-form input.remove_button')).click(
+                        function(){
+                        promotion_number = $(this).data("promotion-number");
+                        remove_promotion(promotion_number);
+                    }
+                    );
+                    startFlag = 0;
+                    index = endDate;
+                    parent.prevUntil(".date-start").addClass("date-sel");
+                    add_promotion_details(parent.prevUntil(".date-start"));
+                } else {
+                    alert("Please select a date after the start date");
+                }
 
-        $(".promo-bubble").toggle(false);
-        $("#bubble"+index).toggle();
-      }
-    } else if (parent.is(".date-end")) {
+                $(".promo-bubble").toggle(false);
+                $("#bubble"+index).toggle();
+            }
+        } else if (parent.is(".date-end")) {
 
-        index = parseInt(parent.text().trim());
-        $(".promo-bubble").toggle(false);
-        $("#bubble"+index).toggle();
+            index = parseInt(parent.text().trim());
+            $(".promo-bubble").toggle(false);
+            $("#bubble"+index).toggle();
+        }
+        else{
+            $(".promo-bubble").toggle(false);
+        }
+    });
+
+
+    var clear_promotion_data = function(promotion_number){
+        li_for_a_promotion = $(".promotion_"+promotion_number.toString()).removeClass();
+        promotion_number -= 1;
     }
-  });
+
+
+    var add_promotion_details = function(parent){
+        parent.addClass("promotion_" + promotion_number.toString());
+        parent.data("promotion-number",promotion_number)
+    };
+
+    var remove_promotion = function(promotion_number){
+        $('form.promotion_'+promotion_number.toString()).each(function(){this.reset();});
+        clear_promotion_data(promotion_number);
+        $(".promo-bubble").toggle(false);
+    };
+
 });
-
-$('.clear-btn').click(function(){
-  $('.spinner').each(function(){
-      $(this).html("");
-  });
-    clear_promotion_data();
-});
-
-var clear_promotion_data = function(){
-    for(i=0;i<6;i++){
-        $('#p_percentage'+i).innerHTML("");
-        $('#p_quantity'+i).innerHTML("");
-    }
-}
-
-
-
