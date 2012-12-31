492cda1571fe38b3a9d35699dc1eeba2f79b292a
Pramod N <npramod05@gmail.com>
2012-12-31 17:35:42 +0530

    disallowing ovelapping promotions

--- app/assets/javascripts/admin/script.js
+++ app/assets/javascripts/admin/script.js
@@ -1,6 +1,7 @@
+var startFlag = 0;
 $(document).ready(function() {
     var promotion_number = 0;
-    var startFlag = 0, startDate, endDate,index;
+    var startDate, endDate,index;
     $(".range-sel-box li a").on("click", function(){
         var parent = $(this).parent();
 
@@ -14,27 +15,34 @@ $(document).ready(function() {
             } else {
                 endDate = parseInt(parent.text().trim());
 
-                if(endDate > startDate) {
-                    parent.addClass("date-end");
-                    add_promotion_details(parent);
-                    add_promotion_details(parent.find('form.promo-form'));
-                    add_promotion_details(parent.find('form.promo-form input.remove_button'));
-                    $(parent.find('form.promo-form input.remove_button')).click(
-                        function(){
-                        promotion_number = $(this).data("promotion-number");
-                        remove_promotion(promotion_number);
+                if(endDate > startDate ) {
+                    if(!is_overlapping(parent)){
+                        parent.addClass("date-end");
+                        add_promotion_details(parent);
+                        add_promotion_details(parent.find('form.promo-form'));
+                        add_promotion_details(parent.find('form.promo-form input.remove_button'));
+                        $(parent.find('form.promo-form input.remove_button')).click(
+                            function(){
+                            promotion_number = $(this).data("promotion-number");
+                            remove_promotion(promotion_number);
+                        }
+                        );
+                        startFlag = 0;
+                        index = endDate;
+                        parent.prevUntil(".date-start").addClass("date-sel");
+                        add_promotion_details(parent.prevUntil(".date-start"));
                     }
-                    );
-                    startFlag = 0;
-                    index = endDate;
-                    parent.prevUntil(".date-start").addClass("date-sel");
-                    add_promotion_details(parent.prevUntil(".date-start"));
-                } else {
+                    else{
+                        alert("Overlaping promotions not allowed");
+                    }
+                    $(".promo-bubble").toggle(false);
+                    $("#bubble"+index).toggle();
+
+                }
+                else {
                     alert("Please select a date after the start date");
                 }
 
-                $(".promo-bubble").toggle(false);
-                $("#bubble"+index).toggle();
             }
         } else if (parent.is(".date-end")) {
 
@@ -47,6 +55,15 @@ $(document).ready(function() {
         }
     });
 
+    var is_overlapping = function(element){
+        list_of_inbetween_li = element.prevUntil(".date-start");
+        for(i=0; i< list_of_inbetween_li.length ; i++){
+            $target = $(list_of_inbetween_li[i]);
+            if($target.is(".date-sel") || $target.is(".date-end") || $target.is(".date-start"))
+                return true;
+        }
+        return false;
+    };
 
     var clear_promotion_data = function(promotion_number){
         li_for_a_promotion = $(".promotion_"+promotion_number.toString()).removeClass();
