c796b724ec91c294fe45006e36425078d8b19104
archanar <archupsgtech@gmail.com>
2013-01-07 19:03:38 +0530

    Cleaning up report color coding

--- app/assets/javascripts/admin/promotion_periodic_stats.js
+++ app/assets/javascripts/admin/promotion_periodic_stats.js
@@ -16,37 +16,31 @@ var show_sales_statistics = function (report) {
 
 
     total_sales.innerHTML = report.total_sales + " " + "(" + report.growth_over_previous_period + "%" + ")"+"*"
-    if (report.total_sales > report.target_sales)
-      total_sales.className = "red-report"; 
-    else
-      total_sales.className = "green-report";
-
+    color_code_report(total_sales,report.target_sales, report.total_sales);
     target_sales.innerHTML = report.target_sales 
     reveneue_variation.innerHTML = report.revenue_variation 
     gross_profit.innerHTML = report.gross_profit
-    if (gross_profit.total_sales < 0)
-      gross_profit.className = "red-report"; 
-    else
-      gross_profit.className = "green-report";
+    color_code_report(gross_profit, report.gross_profit,0);
 
     Stock_out_date_before_promotion.innerHTML = report.stock_out_date_before_promotion
     if (mode == "simulation") {
-        simulated_revenue.innerHTML = report.simulated_revenue
-        target_sales_in_simulation.innerHTML = report.target_sales
-        if (report.simulated_revenue > report.simulated_revenue)
-          simulated_revenue.className = "red-report"; 
-        else
-          simulated_revenue.className = "green-report";
-        simulated_profit.innerHTML = report.simulated_profit
-        if (simulated_profit.total_sales < 0)
-          simulated_profit.className = "red-report"; 
-        else
-          simulated_profit.className = "green-report";
-        
-        revenue_change.innerHTML = report.promotional_revenue_change
-        profit_change.innerHTML = report.promotional_profit_change
-        simulated_revenue_variation.innerHTML = report.simulated_revenue_variation
-        stock_out_date.innerHTML = report.stock_out_date
+      simulated_revenue.innerHTML = report.simulated_revenue
+      target_sales_in_simulation.innerHTML = report.target_sales
+      color_code_report(target_sales_in_simulation, report.target_sales, report.simulated_revenue);
+      simulated_profit.innerHTML = report.simulated_profit
+      color_code_report(simulated_profit, report.simulated_profit,0);
+
+      revenue_change.innerHTML = report.promotional_revenue_change
+      profit_change.innerHTML = report.promotional_profit_change
+      simulated_revenue_variation.innerHTML = report.simulated_revenue_variation
+      stock_out_date.innerHTML = report.stock_out_date
     }
+};
+
+var color_code_report = function(reportElement, value, valueToCompare){
+  if(value < valueToCompare)
+    reportElement.className = "red-report";
+  else if(valueToCompare < value)
+    reportElement.className = "green-report"
 };
 
