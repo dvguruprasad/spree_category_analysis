6abd378d1c0b0ddda0820e74accaace51ffbb61a
Pramod N <npramod05@gmail.com>
2013-01-08 11:46:04 +0530

    routes to reflect better overrides

--- app/overrides/all.rb
+++ app/overrides/all.rb
@@ -1,10 +1,10 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "promotion_simulator",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => '<li><a href="/admin/promotion_simulator/">Promotion Simulator</a></li>')
+                     :text => '<%= tab :promotion_simulator, :route => :promotion_simulator %>')
 #TODO: fix the promo over ride to point to a product list page and not a specific product
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "assortment_map",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => '<li><a href="/admin/assortment_map">Category Analysis</a></li>')
+                     :text => '<%= tab :category_analysis, :route => :category_analysis %>')
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Spree::Core::Engine.routes.draw do
-    get "admin/assortment_map/", :to => "admin/assortment_map#index"
+    get "admin/assortment_map/", :to => "admin/assortment_map#index" , :as => :category_analysis
     get "admin/assortment_map/:taxon_id/:week", :constraints => { :taxon_id => /\d+/ ,:week => /[-]?\d+/},:to => "admin/assortment_map#show"
 
     get "admin/promotion_simulator/product/:product_id/:day/:month/:year", :constraints => { :year => /\d{4}/, :month => /\d{1,2}/, :day => /\d{1,2}/ }, :to => "admin/promotion_simulator#index"
     get "admin/promotion_simulator/product/:product_id/", :to => "admin/promotion_simulator#index"
     get "admin/promotion_simulator/simulate/", :to => "admin/promotion_simulator#simulate"
-    get "admin/promotion_simulator/", :to => "admin/promotion_simulator#landing_page"
+    get "admin/promotion_simulator/", :to => "admin/promotion_simulator#landing_page", :as => :promotion_simulator
 
 end
